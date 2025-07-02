dead2c05d4ce532d8e937048fb1e442461243f17
Juan Esteban Acevedo <124696997+JuanesAcevedoP@users.noreply.github.com>
2025-07-02 10:41:37 -0500

    Cors 2.0

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(cors({
   origin: [
     'http://127.0.0.1:5500',
     'http://localhost:5500',
-    'https://admin-oriente.vercel.app/'
+    'https://admin-oriente.vercel.app'
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
